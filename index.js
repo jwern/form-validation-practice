804ca0452a96bdb9ea349acd91e601a32f51793a
Jillian Werner <jillianlwerner@gmail.com>
2021-03-15 14:37:27 -0400

    Add password match validation; adjust error position on mobile

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
 const form = document.getElementById('user-info');
 const emailInput = form.querySelector('#mail');
+const inputs = form.querySelectorAll('.user-input');
 
-const showErrorMessage = function(invalidInput) {
-  const inputErrorSpan = invalidInput.closest('.input-group').querySelector('.error');
-  inputErrorSpan.textContent = "An error!";
+const fillErrorSpan = function(input, message) {
+  const inputErrorSpan = input.closest('.input-group').querySelector('.error');
+  inputErrorSpan.textContent = `${message}`;
+}
+
+// const showEmailErrors = function(invalidInput) {
+//   fillErrorSpan(invalidInput, "Please enter a valid email");
+// }
+
+// const showZipErrors = function(invalidInput) {
+//   fillErrorSpan(invalidInput, "Please enter a 5-digit zip code");
+// }
+
+const clearErrorMessages = function(validInput) {
+  fillErrorSpan(validInput, "");
+}
+
+const selectErrorMessage = function(invalidInput) {
+  const errorMessages = {
+    email: "Please enter a valid email",
+    zipcode: "Please enter a 5-digit zip code",
+    country: "Country should only contain letters",
+    password: "Password must be a minimum of 8 characters and contain at least one each of a letter, number, and special character",
+    "password-confirm": "Passwords must match",
+  }
+
+  fillErrorSpan(invalidInput, errorMessages[invalidInput.name]);
+}
+
+const checkPasswordValidity = function(input) {
+  const userPassword = document.getElementById('pass').value
+  return input.value === userPassword;
 }
 
 const checkValidity = function() {
-  if (!this.validity.valid) {
-    showErrorMessage(this);
+  let inputValid = this.validity.valid;
+
+  if (this.name === "password-confirm") {
+    inputValid = checkPasswordValidity(this);
+  } 
+  
+  if (!inputValid) {
+    selectErrorMessage(this);
+  } else {
+    clearErrorMessages(this);
   }
 }
 
@@ -27,12 +65,18 @@ const checkForm = function(event) {
   let errors = [];
 
   inputs.forEach(input => {
-    if (!input.validity.valid) {
+    let inputValid = input.validity.valid;
+
+    if (input.name === "password-confirm") {
+      inputValid = checkPasswordValidity(input);
+    }
+
+    if (!inputValid) {
       errors.push(`Error: invalid ${input.name} input`);
     }
   });
 
-  if (errors) {
+  if (errors.length > 0) {
     event.preventDefault();
     errors.forEach(err => {
       let errDiv = document.createElement('div');
@@ -43,4 +87,5 @@ const checkForm = function(event) {
 }
 
 form.addEventListener('submit', event => checkForm(event));
-emailInput.addEventListener('input', checkValidity);
+// emailInput.addEventListener('input', checkValidity);
+inputs.forEach(input => input.addEventListener('input', checkValidity));
